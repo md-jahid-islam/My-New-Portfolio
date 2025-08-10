a4ba88ea25bf1bbfde58509f7b1c04174a3be2f1
md-jahid-islam <jahidulislamwebbd@gmail.com>
2025-08-10 22:49:48 +0600

    dsf

--- src/Components/BlogComponents.jsx
+++ src/Components/BlogComponents.jsx
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
- const BlogComponents = () => {
  //========== useEffect AOS duration start =========//
+ const BlogComponents = () => {
  useEffect(() => {
    AOS.init({ duration: 1000 });
  }, []);
